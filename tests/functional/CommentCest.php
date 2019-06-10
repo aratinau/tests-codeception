54cb4fe2de2d7b9ac9826cefa83e04111c096e0c
Alex Kunin <alexkunin@gmail.com>
2019-06-10 18:27:07 +0300

    Test deep association queries in Doctrine2::seeInRepository

--- tests/functional/CommentCest.php
+++ tests/functional/CommentCest.php
@@ -21,7 +21,31 @@ class CommentCest
         $I->submitForm('#post-add-comment > form', []);
         $I->seeCurrentRouteIs('blog_post');
         $I->see('Hi, Symfony!');
-        $I->seeInRepository(Comment::class, ['content' => 'Hi, Symfony!']);
+        // This particular assertion is a bit of overkill, but it also serves as a testcase
+        // of `seeInRepository` method with complex associations. Here is how this assertion
+        // translates to English:
+        //
+        //   * I see a comment entity with content equal to 'Hi, Symfony!'
+        //   * and 'author' association points to a user entity with username equal to 'john_user'
+        //   ** note: john_user is one who created the comment, as it is used in login
+        //      procedure above
+        //   * and 'post' association points to a post entity which...
+        //   * ...has an associated comment entity which...
+        //   * ...has same content and author with same username.
+        $I->seeInRepository(Comment::class, [
+            'content' => 'Hi, Symfony!',
+            'author' => [
+                'username' => 'john_user',
+            ],
+            'post' => [
+                'comments' => [
+                    'content' => 'Hi, Symfony!',
+                    'author' => [
+                        'username' => 'john_user'
+                    ]
+                ]
+            ]
+        ]);
     }
 
 }
